09e9afe8320919833cf7b058b5f33f6c3df90261
Jesse Plamondon-Willard <github@jplamondonw.com>
2016-08-22 00:10:35 -0400

    fix error when showing lookup UI in some maps

--- LookupAnything/Components/Sprites.cs
+++ LookupAnything/Components/Sprites.cs
@@ -16,7 +16,7 @@ namespace Pathoschild.LookupAnything.Components
         public static class Letter
         {
             /// <summary>The sprite sheet containing the letter sprites.</summary>
-            public static Texture2D Sheet => Game1.temporaryContent.Load<Texture2D>("LooseSprites\\letterBG");
+            public static Texture2D Sheet => Game1.content.Load<Texture2D>("LooseSprites\\letterBG");
 
             /// <summary>The letter background (including edges and corners).</summary>
             public static readonly Rectangle Sprite = new Rectangle(0, 0, 320, 180);
