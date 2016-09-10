04e24ecaefed32aca105186301619ee4266115e5
Jesse Plamondon-Willard <github@jplamondonw.com>
2016-09-10 17:22:16 -0400

    correct targeting for monsters

--- LookupAnything/Framework/Targets/CharacterTarget.cs
+++ LookupAnything/Framework/Targets/CharacterTarget.cs
@@ -1,6 +1,7 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using StardewValley;
+using StardewValley.Monsters;
 
 namespace Pathoschild.LookupAnything.Framework.Targets
 {
@@ -21,13 +22,31 @@ namespace Pathoschild.LookupAnything.Framework.Targets
         public override Rectangle GetSpriteArea()
         {
             NPC npc = (NPC)this.Value;
-            var boundingBox = npc.GetBoundingBox(); // the 'occupied' area at the animal's feet
+            var boundingBox = npc.GetBoundingBox(); // the 'occupied' area at the NPC's feet
 
+            // calculate y origin
+            float yOrigin;
+            if (npc is DustSpirit)
+                yOrigin = boundingBox.Bottom;
+            else if (npc is Bat)
+                yOrigin = boundingBox.Center.Y;
+            else if (npc is Bug)
+                yOrigin = boundingBox.Top - npc.sprite.spriteHeight * Game1.pixelZoom + (float)(System.Math.Sin(Game1.currentGameTime.TotalGameTime.Milliseconds / 1000.0 * (2.0 * System.Math.PI)) * 10.0);
+            else if (npc is SquidKid)
+            {
+                int yOffset = GameHelper.GetPrivateField<int>((SquidKid)npc, "yOffset");
+                yOrigin = boundingBox.Bottom - npc.sprite.spriteHeight * Game1.pixelZoom + yOffset;
+            }
+            else
+                yOrigin = boundingBox.Top;
+
+            // get bounding box
             int height = npc.sprite.spriteHeight * Game1.pixelZoom;
             int width = npc.sprite.spriteWidth * Game1.pixelZoom;
-            int x = boundingBox.Center.X - (width / 2);
-            int y = boundingBox.Y + boundingBox.Height - height;
-            return new Rectangle(x - Game1.viewport.X, y - Game1.viewport.Y, width, height);
+            float x = boundingBox.Center.X - (width / 2);
+            float y = yOrigin + boundingBox.Height - height + npc.yJumpOffset * 2;
+
+            return new Rectangle((int)(x - Game1.viewport.X), (int)(y - Game1.viewport.Y), width, height);
         }
 
         /// <summary>Get whether the visible sprite intersects the specified coordinate. This can be an expensive test.</summary>
@@ -37,6 +56,15 @@ namespace Pathoschild.LookupAnything.Framework.Targets
         public override bool SpriteIntersectsPixel(Vector2 tile, Vector2 position, Rectangle spriteArea)
         {
             NPC npc = (NPC)this.Value;
+
+            // allow any part of the sprite area for monsters
+            // (Monsters have complicated and inconsistent sprite behaviour which isn't really
+            // worth reverse-engineering, and sometimes move around so much that a pixel-perfect
+            // check is inconvenient anyway.)
+            if (npc is Monster)
+                return spriteArea.Contains((int)position.X, (int)position.Y);
+
+            // check sprite for non-monster NPCs
             SpriteEffects spriteEffects = npc.flip ? SpriteEffects.FlipHorizontally : SpriteEffects.None;
             return this.SpriteIntersectsPixel(tile, position, spriteArea, npc.sprite.Texture, npc.sprite.sourceRect, spriteEffects);
         }
