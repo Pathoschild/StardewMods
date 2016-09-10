e93433124baa4358744cb5415f5828f325e113d0
Jesse Plamondon-Willard <github@jplamondonw.com>
2016-09-10 14:18:07 -0400

    correct targeting for fences

--- LookupAnything/Framework/Targets/ObjectTarget.cs
+++ LookupAnything/Framework/Targets/ObjectTarget.cs
@@ -26,6 +26,11 @@ namespace Pathoschild.LookupAnything.Framework.Targets
                 Rectangle sourceRectangle = Object.getSourceRectForBigCraftable(obj.parentSheetIndex);
                 return new Rectangle(tileRectangle.X, tileRectangle.Y - (sourceRectangle.Height * Game1.pixelZoom) + tileRectangle.Height, sourceRectangle.Width * Game1.pixelZoom, sourceRectangle.Height * Game1.pixelZoom);
             }
+            else if (obj is Fence)
+            {
+                Rectangle sourceRectangle = this.GetSourceRectangle((Fence)obj, Game1.currentLocation);
+                return new Rectangle(tileRectangle.X, tileRectangle.Y - (sourceRectangle.Height * Game1.pixelZoom) + tileRectangle.Height, sourceRectangle.Width * Game1.pixelZoom, sourceRectangle.Height * Game1.pixelZoom);
+            }
             else
             {
                 Rectangle sourceRectangle = Game1.getSourceRectForStandardTileSheet(Game1.objectSpriteSheet, obj.parentSheetIndex);
@@ -41,17 +46,76 @@ namespace Pathoschild.LookupAnything.Framework.Targets
         {
             Object obj = (Object)this.Value;
 
-            // get sprite sheet
-            Texture2D spriteSheet = obj.bigCraftable
-                ? Game1.bigCraftableSpriteSheet
-                : Game1.objectSpriteSheet;
-            Rectangle sourceRectangle = obj.bigCraftable
-                ? Object.getSourceRectForBigCraftable(obj.parentSheetIndex)
-                : Game1.currentLocation.getSourceRectForObject(obj.ParentSheetIndex);
+            // get sprite data
+            Texture2D spriteSheet;
+            Rectangle sourceRectangle;
+            if (obj.bigCraftable)
+            {
+                spriteSheet = Game1.bigCraftableSpriteSheet;
+                sourceRectangle = Object.getSourceRectForBigCraftable(obj.parentSheetIndex);
+            }
+            else if (obj is Fence)
+            {
+                spriteSheet = GameHelper.GetPrivateField<Texture2D>(obj, "fenceTexture");
+                sourceRectangle = this.GetSourceRectangle((Fence)obj, Game1.currentLocation);
+            }
+            else
+            {
+                spriteSheet = Game1.objectSpriteSheet;
+                sourceRectangle = Game1.currentLocation.getSourceRectForObject(obj.ParentSheetIndex);
+            }
 
             // check pixel from sprite sheet
             SpriteEffects spriteEffects = obj.flipped ? SpriteEffects.FlipHorizontally : SpriteEffects.None;
             return this.SpriteIntersectsPixel(tile, position, spriteArea, spriteSheet, sourceRectangle, spriteEffects);
+        }
+
+        /// <summary>Get the source rectangle for a fence texture.</summary>
+        /// <param name="fence">The fence object.</param>
+        /// <param name="location">The location containing the fence target.</param>
+        /// <remarks>Reverse-engineered from <see cref="Fence.draw(SpriteBatch,int,int,float)"/>.</remarks>
+        private Rectangle GetSourceRectangle(Fence fence, GameLocation location)
+        {
+            int spriteID = 1;
+            if (fence.health > 1.0)
+            {
+                int index = 0;
+                Vector2 tile = fence.tileLocation;
+
+                // connected to right fence
+                tile.X += 1;
+                if (location.objects.ContainsKey(tile) && location.objects[tile].GetType() == typeof(Fence) && ((Fence)location.objects[tile]).countsForDrawing(fence.whichType))
+                    index += 100;
+
+                // connected to left fence
+                tile.X -= 2;
+                if (location.objects.ContainsKey(tile) && location.objects[tile].GetType() == typeof(Fence) && ((Fence)location.objects[tile]).countsForDrawing(fence.whichType))
+                    index += 10;
+
+                // connected to top fence
+                tile.X += 1;
+                tile.Y += 1;
+                if (location.objects.ContainsKey(tile) && location.objects[tile].GetType() == typeof(Fence) && ((Fence)location.objects[tile]).countsForDrawing(fence.whichType))
+                    index += 500;
+
+                // connected to bottom fence
+                tile.Y -= 2;
+                if (location.objects.ContainsKey(tile) && location.objects[tile].GetType() == typeof(Fence) && ((Fence)location.objects[tile]).countsForDrawing(fence.whichType))
+                    index += 1000;
+                if (fence.isGate)
+                {
+                    if (index == 110)
+                        return new Rectangle(fence.gatePosition == Fence.gateOpenedPosition ? 24 : 0, 128, 24, 32);
+                    if (index == 1500)
+                        return new Rectangle(fence.gatePosition == Fence.gateClosedPosition ? 16 : 0, 160, 16, 16);
+                    spriteID = Fence.sourceRectForSoloGate;
+                }
+                else
+                    spriteID = Fence.fenceDrawGuide[index];
+            }
+
+            Texture2D texture = GameHelper.GetPrivateField<Texture2D>(fence, "fenceTexture");
+            return new Rectangle(spriteID * Fence.fencePieceWidth % texture.Bounds.Width, spriteID * Fence.fencePieceWidth / texture.Bounds.Width * Fence.fencePieceHeight, Fence.fencePieceWidth, Fence.fencePieceHeight);
         }
     }
 }
