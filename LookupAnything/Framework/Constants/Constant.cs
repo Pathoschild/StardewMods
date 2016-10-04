a4513583b83e235c7a7deac1b170bdcab03192c2
Jesse Plamondon-Willard <github@jplamondonw.com>
2016-10-04 14:36:40 -0400

    migrate to Stardew Valley 1.1

--- LookupAnything/Framework/Constants/Constant.cs
+++ LookupAnything/Framework/Constants/Constant.cs
@@ -10,10 +10,10 @@ namespace Pathoschild.LookupAnything.Framework.Constants
         ** Accessors
         *********/
         /// <summary>The minimum supported version of Stardew Valley.</summary>
-        public static string MinimumGameVersion = "1.07";
+        public static string MinimumGameVersion = "1.1";
 
         /// <summary>The minimum supported version of SMAPI.</summary>
-        public static string MinimumApiVersion = "0.40";
+        public static string MinimumApiVersion = "0.40.0 1.1";
 
         /// <summary>The largest expected sprite size (measured in tiles).</summary>
         /// <remarks>This is used to account for sprites that extend beyond their tile when searching for targets. These values should be large enough to cover the largest target sprites, but small enough to minimise expensive cursor collision checks.</remarks>
--- README.md
+++ README.md
@@ -86,6 +86,9 @@ file if you want. These are the available settings:
 * Fixed fields being shown for dead crops.
 * Internal refactoring.
 
+1.4:
+* Updated for Stardew Valley 1.1.
+
 ## Compiling the mod
 [Installing a stable release from Nexus Mods](http://www.nexusmods.com/stardewvalley/mods/541/) is
 recommended. If you really want to compile the mod yourself, just edit `LookupAnything.csproj` and
