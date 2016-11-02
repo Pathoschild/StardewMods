9bb5254c787722f0e2022976ebbf064c07ebc1e9
Jesse Plamondon-Willard <github@jplamondonw.com>
2016-11-01 23:53:15 -0400

    create scaffolded mod project

---
+++ DebugMode/ModEntry.cs
@@ -0,0 +1,16 @@
+﻿using StardewModdingAPI;
+
+namespace Pathoschild.Stardew.DebugMode
+{
+    /// <summary>The mod entry point.</summary>
+    public class ModEntry : Mod
+    {
+        /*********
+        ** Public methods
+        *********/
+        /// <summary>Initialise the mod.</summary>
+        public override void Entry(params object[] objects)
+        {
+        }
+    }
+}
---
+++ DebugMode/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Pathoschild.Stardew.DebugMode")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyProduct("Pathoschild.Stardew.DebugMode")]
+[assembly: ComVisible(false)]
+[assembly: Guid("f3e43047-a0ef-4240-a75c-49cf01952f32")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
