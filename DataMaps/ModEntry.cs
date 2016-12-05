cfca88923729ff9913af4f3165ad890dad61ad5f
Jesse Plamondon-Willard <github@jplamondonw.com>
2016-12-05 00:09:28 -0500

    add toggle key with config and error-handling

--- DataMaps/ModEntry.cs
+++ DataMaps/ModEntry.cs
@@ -1,4 +1,6 @@
-﻿using StardewModdingAPI;
+﻿using Pathoschild.Stardew.DataMaps.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
 
 namespace Pathoschild.Stardew.DataMaps
 {
@@ -6,13 +8,53 @@ namespace Pathoschild.Stardew.DataMaps
     public class ModEntry : Mod
     {
         /*********
+        ** Properties
+        *********/
+        /// <summary>The mod configuration.</summary>
+        private ModConfig Config;
+
+
+        /*********
         ** Public methods
         *********/
         /// <summary>The mod entry point, called after the mod is first loaded.</summary>
         /// <param name="helper">Provides methods for interacting with the mod directory, such as read/writing a config file or custom JSON files.</param>
         public override void Entry(IModHelper helper)
         {
-            // TODO
+            // load config
+            this.Config = helper.ReadConfig<RawModConfig>().GetParsed();
+
+            // hook up events
+            if (this.Config.Keyboard.HasAny())
+                ControlEvents.KeyPressed += (sender, e) => this.ReceiveKeyPress(e.KeyPressed, this.Config.Keyboard);
+
+            // hook up controller
+            if (this.Config.Controller.HasAny())
+            {
+                ControlEvents.ControllerButtonPressed += (sender, e) => this.ReceiveKeyPress(e.ButtonPressed, this.Config.Controller);
+                ControlEvents.ControllerTriggerPressed += (sender, e) => this.ReceiveKeyPress(e.ButtonPressed, this.Config.Controller);
+            }
+        }
+
+
+        /*********
+        ** Private methods
+        *********/
+        /// <summary>The method invoked when the player presses an input button.</summary>
+        /// <typeparam name="TKey">The input type.</typeparam>
+        /// <param name="key">The pressed input.</param>
+        /// <param name="map">The configured input mapping.</param>
+        private void ReceiveKeyPress<TKey>(TKey key, InputMapConfiguration<TKey> map)
+        {
+            if (!map.IsValidKey(key))
+                return;
+
+            // perform bound action
+            this.Monitor.InterceptErrors("handling your input", $"handling input '{key}'", () =>
+            {
+                if (key.Equals(map.ToggleMap))
+                    this.Monitor.Log("toggled map");
+            });
         }
     }
 }
