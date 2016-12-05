e5a620c6de7433846e7f0ed37d6a5d7de6fa8cd9
Jesse Plamondon-Willard <github@jplamondonw.com>
2016-12-04 23:56:22 -0500

    create basic mod project with readme

---
+++ DataMaps/ModEntry.cs
@@ -0,0 +1,18 @@
+﻿using StardewModdingAPI;
+
+namespace Pathoschild.Stardew.DataMaps
+{
+    /// <summary>The mod entry point.</summary>
+    public class ModEntry : Mod
+    {
+        /*********
+        ** Public methods
+        *********/
+        /// <summary>The mod entry point, called after the mod is first loaded.</summary>
+        /// <param name="helper">Provides methods for interacting with the mod directory, such as read/writing a config file or custom JSON files.</param>
+        public override void Entry(IModHelper helper)
+        {
+            // TODO
+        }
+    }
+}
---
+++ DataMaps/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Pathoschild.Stardew.DataMaps")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCopyright("Copyright © 2016")]
+[assembly: Guid("cdeef8af-15cb-4955-8163-e446f212c2f4")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
