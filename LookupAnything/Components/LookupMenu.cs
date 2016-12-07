353cee98163c1f67cad5f89eaac16b6025bd41a3
ShadowDragon <shadowxdragonx42@gmail.com>
2016-12-06 17:17:35 -0800

    Change word.

--- LookupAnything/Components/LookupMenu.cs
+++ LookupAnything/Components/LookupMenu.cs
@@ -100,7 +100,7 @@ namespace Pathoschild.Stardew.LookupAnything.Components
             this.CalculateDimensions();
         }
 
-        /// <summary>The method called when the User presses a controller button.</summary>
+        /// <summary>The method called when the player presses a controller button.</summary>
         /// <param name="button">The controller button pressed.</param>
         public override void receiveGamePadButton(Buttons button)
         {
