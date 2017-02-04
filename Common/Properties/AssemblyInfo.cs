29a8fb6665a0aabbc6603d882bf9ae0ec02f11fa
Jesse Plamondon-Willard <github@jplamondonw.com>
2017-02-04 16:06:31 -0500

    convert common to shared project

--- Common/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Pathoschild.Stardew.Common")]
-[assembly: Guid("d781a4da-11dd-4c47-8c68-9b014cb69045")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
