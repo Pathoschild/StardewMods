3064126459c545b55baa52dad9cd3dde4ee13d75
Jesse Plamondon-Willard <github@jplamondonw.com>
2017-02-05 13:59:46 -0500

    update for 1.3 release

--- SkipIntro/Properties/AssemblyInfo.cs
+++ SkipIntro/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Pathoschild.Stardew.SkipIntro")]
 [assembly: Guid("56fb522e-923d-4582-ad3d-8ec80f09237a")]
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyVersion("1.3.0.0")]
+[assembly: AssemblyFileVersion("1.3.0.0")]
