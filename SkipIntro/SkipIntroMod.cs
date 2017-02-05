7efc40c677a94ed839add461ab0b04bada5ef4ec
Jesse Plamondon-Willard <github@jplamondonw.com>
2017-02-05 12:37:47 -0500

    add update check

--- SkipIntro/SkipIntroMod.cs
+++ SkipIntro/SkipIntroMod.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Reflection;
+using System.Threading.Tasks;
+using Pathoschild.Stardew.Common;
+using Pathoschild.Stardew.SkipIntro.Framework;
 using StardewModdingAPI;
 using StardewModdingAPI.Events;
 using StardewValley.Menus;
@@ -10,19 +13,44 @@ namespace Pathoschild.Stardew.SkipIntro
     public class SkipIntroMod : Mod
     {
         /*********
+        ** Properties
+        *********/
+        /// <summary>The mod configuration.</summary>
+        private ModConfig Config;
+
+
+        /*********
         ** Public methods
         *********/
         /// <summary>The mod entry point, called after the mod is first loaded.</summary>
         /// <param name="helper">Provides methods for interacting with the mod directory, such as read/writing a config file or custom JSON files.</param>
         public override void Entry(IModHelper helper)
         {
+            this.Config = helper.ReadConfig<ModConfig>();
+
             MenuEvents.MenuChanged += this.ReceiveMenuChanged;
+            GameEvents.GameLoaded += this.ReceiveGameLoaded;
         }
 
 
         /*********
         ** Private methods
         *********/
+        /// <summary>The method invoked when the player loads the game.</summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event arguments.</param>
+        private void ReceiveGameLoaded(object sender, EventArgs e)
+        {
+            // check for an updated version
+            if (this.Config.CheckForUpdates)
+            {
+                Task.Factory.StartNew(() =>
+                {
+                    UpdateHelper.LogVersionCheck(this.Monitor, this.ModManifest.Version, "SkipIntro").Wait();
+                });
+            }
+        }
+
         /// <summary>The method invoked when the game replaces one menu with another.</summary>
         /// <param name="sender">The event sender.</param>
         /// <param name="e">The event arguments.</param>
--- SkipIntro/release-notes.md
+++ SkipIntro/release-notes.md
@@ -2,6 +2,11 @@
 
 # Release notes
 
+## 1.3
+See log [part A](https://github.com/Pathoschild/StardewMods/compare/skip-intro/1.2...6b1b3f8fd70889d6723a43893ea8b9446fdce2fa) and [part B](https://github.com/Pathoschild/StardewMods/compare/5eb97b03b31135e8038840669edc4bb36e792467...skip-intro/1.3).
+
+* Added update check.
+
 ## 1.2
 See [log](https://github.com/Pathoschild/StardewMods/compare/skip-intro/1.1...skip-intro/1.2).
 
