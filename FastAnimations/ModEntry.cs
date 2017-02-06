53a4a4dc02cfa6ed54a8102cbefb1736f0214971
Jesse Plamondon-Willard <github@jplamondonw.com>
2017-02-05 20:54:55 -0500

    add skeleton mod project for FastAnimations

---
+++ FastAnimations/ModEntry.cs
@@ -0,0 +1,22 @@
+﻿using StardewModdingAPI;
+
+namespace Pathoschild.Stardew.FastAnimations
+{
+    /// <summary>The mod entry point.</summary>
+    public class SkipIntroMod : Mod
+    {
+        /*********
+        ** Public methods
+        *********/
+        /// <summary>The mod entry point, called after the mod is first loaded.</summary>
+        /// <param name="helper">Provides methods for interacting with the mod directory, such as read/writing a config file or custom JSON files.</param>
+        public override void Entry(IModHelper helper)
+        {
+        }
+
+
+        /*********
+        ** Private methods
+        *********/
+    }
+}
---
+++ FastAnimations/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Pathoschild.Stardew.FastAnimations")]
+[assembly: Guid("ea68e967-be31-4231-a75b-2c9be8e91d9f")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
