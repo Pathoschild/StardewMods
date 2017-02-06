dedff0ad576ed198be7cb4af8fc8bfda6bbd4f45
Jesse Plamondon-Willard <github@jplamondonw.com>
2017-02-06 01:15:49 -0500

    let game update between skip steps

--- SkipIntro/SkipIntroMod.cs
+++ SkipIntro/SkipIntroMod.cs
@@ -4,6 +4,7 @@ using Pathoschild.Stardew.Common;
 using Pathoschild.Stardew.SkipIntro.Framework;
 using StardewModdingAPI;
 using StardewModdingAPI.Events;
+using StardewValley;
 using StardewValley.Menus;
 
 namespace Pathoschild.Stardew.SkipIntro
@@ -17,6 +18,9 @@ namespace Pathoschild.Stardew.SkipIntro
         /// <summary>The mod configuration.</summary>
         private ModConfig Config;
 
+        /// <summary>The update ticks that have been processed.</summary>
+        private int Step;
+
 
         /*********
         ** Public methods
@@ -27,14 +31,17 @@ namespace Pathoschild.Stardew.SkipIntro
         {
             this.Config = helper.ReadConfig<ModConfig>();
 
-            MenuEvents.MenuChanged += this.ReceiveMenuChanged;
             GameEvents.GameLoaded += this.ReceiveGameLoaded;
+            GameEvents.EighthUpdateTick += this.ReceiveUpdateTick;
         }
 
 
         /*********
         ** Private methods
         *********/
+        /****
+        ** Event handlers
+        ****/
         /// <summary>The method invoked when the player loads the game.</summary>
         /// <param name="sender">The event sender.</param>
         /// <param name="e">The event arguments.</param>
@@ -50,26 +57,55 @@ namespace Pathoschild.Stardew.SkipIntro
             }
         }
 
-        /// <summary>The method invoked when the game replaces one menu with another.</summary>
+        /// <summary>Receives an update tick.</summary>
         /// <param name="sender">The event sender.</param>
         /// <param name="e">The event arguments.</param>
-        private void ReceiveMenuChanged(object sender, EventArgsClickableMenuChanged e)
+        private void ReceiveUpdateTick(object sender, EventArgs e)
         {
-            this.Monitor.InterceptErrors("skipping the menu", () =>
+            try
             {
-                // get menu
-                TitleMenu menu = e.NewMenu as TitleMenu;
-                if (menu == null)
-                    return;
+                TitleMenu menu = Game1.activeClickableMenu as TitleMenu;
+                if (menu != null)
+                {
+                    if (!this.ApplySkip(menu, this.Step))
+                        GameEvents.UpdateTick -= this.ReceiveUpdateTick;
+                    this.Step++;
+                }
+            }
+            catch (Exception ex)
+            {
+                this.Monitor.InterceptError(ex, $"skipping the menu (step {this.Step})");
+                GameEvents.UpdateTick -= this.ReceiveUpdateTick;
+            }
+        }
 
-                // skip intro
-                this.Helper.Reflection.GetPrivateField<int>(menu, "chuckleFishTimer").SetValue(0);
-                menu.skipToTitleButtons();
+        /****
+        ** Methods
+        ****/
+        /// <summary>Apply the next skip step.</summary>
+        /// <param name="menu">The title menu to update.</param>
+        /// <param name="step">The step to apply (starting at 0).</param>
+        /// <returns>Returns whether there are more skip steps.</returns>
+        /// <remarks>The skip logic is applied over several update ticks to let the game update itself smoothly. This prevents a few issues like a long pause before the game window opens, or the title menu not resizing itself for full-screen display.</remarks>
+        private bool ApplySkip(TitleMenu menu, int step)
+        {
+            switch (step)
+            {
+                // skip to main menu
+                case 1:
+                    this.Helper.Reflection.GetPrivateField<int>(menu, "chuckleFishTimer").SetValue(0);
+                    menu.skipToTitleButtons();
+                    return true;
 
-                // skip to load screen
-                if (this.Config.SkipToLoadScreen)
-                    menu.performButtonAction("Load");
-            });
+                // skip to loading screen
+                case 2:
+                    if (this.Config.SkipToLoadScreen)
+                        menu.performButtonAction("Load");
+                    return true;
+
+                default:
+                    return false;
+            }
         }
     }
 }
