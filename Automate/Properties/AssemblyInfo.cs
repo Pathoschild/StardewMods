76c53099ce6938d69ba0491bb036978e547746ca
Jesse Plamondon-Willard <github@jplamondonw.com>
2017-08-18 14:25:16 -0400

    update Automate version for release

--- Automate/Properties/AssemblyInfo.cs
+++ Automate/Properties/AssemblyInfo.cs
@@ -1,7 +1,7 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Automate")]
 [assembly: Guid("a72152b0-ab75-44e4-86d8-f6b8b9920b59")]
-[assembly: AssemblyVersion("1.5.1.0")]
-[assembly: AssemblyFileVersion("1.5.1.0")]
+[assembly: AssemblyVersion("1.6.0.0")]
+[assembly: AssemblyFileVersion("1.6.0.0")]
