ab4d41720eb92cc2e31d715875bbd232ecb44751
Jesse Plamondon-Willard <github@jplamondonw.com>
2017-12-07 20:24:54 -0500

    add warp tiles to traversable data map

--- DataMaps/DataMaps/TraversableMap.cs
+++ DataMaps/DataMaps/TraversableMap.cs
@@ -5,6 +5,8 @@ using Pathoschild.Stardew.Common;
 using Pathoschild.Stardew.DataMaps.Framework;
 using StardewValley;
 using xTile.Dimensions;
+using xTile.ObjectModel;
+using xTile.Tiles;
 using Rectangle = Microsoft.Xna.Framework.Rectangle;
 
 namespace Pathoschild.Stardew.DataMaps.DataMaps
@@ -23,6 +25,13 @@ namespace Pathoschild.Stardew.DataMaps.DataMaps
 
         /// <summary>The color for an impassable tile.</summary>
         private readonly Color ImpassableColor = Color.Red;
+
+        /// <summary>The color for a warp tile.</summary>
+        private readonly Color WarpColor = Color.Blue;
+
+        /// <summary>The action tile property values which trigger a warp.</summary>
+        /// <remarks>See remarks on <see cref="IsWarp"/>.</remarks>
+        private readonly HashSet<string> WarpActions = new HashSet<string> { "EnterSewer", "LockedDoorWarp", "Warp", "WarpCommunityCenter", "WarpGreenhouse", "WarpMensLocker", "WarpWomensLocker", "WizardHatch" };
 
 
         /*********
@@ -48,7 +57,8 @@ namespace Pathoschild.Stardew.DataMaps.DataMaps
             {
                 new LegendEntry("Clear", this.ClearColor),
                 new LegendEntry("Occupied", this.OccupiedColor),
-                new LegendEntry("Impassable", this.ImpassableColor)
+                new LegendEntry("Impassable", this.ImpassableColor),
+                new LegendEntry("Warp", this.WarpColor)
             };
         }
 
@@ -78,19 +88,63 @@ namespace Pathoschild.Stardew.DataMaps.DataMaps
         {
             foreach (Vector2 tile in visibleTiles)
             {
-                bool isPassable = location.isTilePassable(new Location((int)tile.X, (int)tile.Y), Game1.viewport);
-                bool isClear = isPassable && !location.isTileOccupiedIgnoreFloors(tile);
-
                 Color color;
-                if (isPassable && isClear)
-                    color = this.ClearColor;
-                else if (isPassable)
-                    color = this.OccupiedColor;
+                if (this.IsWarp(location, tile))
+                    color = this.WarpColor;
+                else if (this.IsPassable(location, tile))
+                    color = this.IsOccupied(location, tile) ? this.OccupiedColor : this.ClearColor;
                 else
                     color = this.ImpassableColor;
 
                 yield return new TileData(tile, color);
             }
+        }
+
+        /// <summary>Get whether there's a warp on the given tile.</summary>
+        /// <param name="location">The current location.</param>
+        /// <param name="tile">The tile to check.</param>
+        /// <remarks>Derived from <see cref="GameLocation.isCollidingWithWarp"/> and <see cref="GameLocation.performAction"/>.</remarks>
+        private bool IsWarp(GameLocation location, Vector2 tile)
+        {
+            // check map warps
+            {
+                Rectangle area = new Rectangle((int)(tile.X * Game1.tileSize), (int)(tile.Y * Game1.tileSize), Game1.tileSize, Game1.tileSize);
+                if (location.isCollidingWithWarpOrDoor(area) != null)
+                    return true;
+            }
+
+            // check tile action
+            {
+                Tile mapTile = location.map.GetLayer("Buildings").PickTile(new Location((int)(tile.X * Game1.tileSize), (int)(tile.Y * Game1.tileSize)), Game1.viewport.Size);
+                if (mapTile != null && mapTile.Properties.TryGetValue("Action", out PropertyValue action) && this.WarpActions.Contains(action))
+                    return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>Get whether a tile is passable.</summary>
+        /// <param name="location">The current location.</param>
+        /// <param name="tile">The tile to check.</param>
+        /// <remarks>Derived from <see cref="Farmer.MovePosition"/> and <see cref="Fence"/>.</remarks>
+        private bool IsPassable(GameLocation location, Vector2 tile)
+        {
+            // tile passable
+            if (location.isTilePassable(new Location((int)tile.X, (int)tile.Y), Game1.viewport))
+                return true;
+
+            return false;
+        }
+
+        /// <summary>Get whether a tile is blocked due to something it contains.</summary>
+        /// <param name="location">The current location.</param>
+        /// <param name="tile">The tile to check.</param>
+        private bool IsOccupied(GameLocation location, Vector2 tile)
+        {
+            if (location.isTileOccupiedIgnoreFloors(tile))
+                return true;
+
+            return false;
         }
     }
 }
