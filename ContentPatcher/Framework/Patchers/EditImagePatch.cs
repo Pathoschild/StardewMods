db0f6cb348ba27b97ece5b019308ccdfe1fb6af1
f4iTh <34132723+f4iTh@users.noreply.github.com>
2018-02-27 00:43:19 +0200

    Update EditImagePatch.cs
    If a file is disabled, we don't want any changes to take place.. right?

--- ContentPatcher/Framework/Patchers/EditImagePatch.cs
+++ ContentPatcher/Framework/Patchers/EditImagePatch.cs
@@ -26,6 +26,9 @@ namespace ContentPatcher.Framework.Patchers
 
         /// <summary>The sprite area to overwrite.</summary>
         private readonly Rectangle? ToArea;
+        
+        /// <summary>Determines if loaded or not.</summary>
+        private readonly bool Enabled;
 
 
         /*********
@@ -52,9 +55,10 @@ namespace ContentPatcher.Framework.Patchers
         /// <param name="fromLocalAsset">The asset key to load from the content pack instead.</param>
         /// <param name="fromArea">The sprite area from which to read an image.</param>
         /// <param name="toArea">The sprite area to overwrite.</param>
+        /// <param name="enabled">Determines if area should be edited.</param>
         /// <param name="monitor">Encapsulates monitoring and logging.</param>
         /// <param name="assetLoader">Handles the logic around loading assets from content packs.</param>
-        public EditImagePatch(IContentPack contentPack, string assetName, string locale, string fromLocalAsset, Rectangle fromArea, Rectangle toArea, IMonitor monitor, AssetLoader assetLoader)
+        public EditImagePatch(IContentPack contentPack, string assetName, string locale, string fromLocalAsset, Rectangle fromArea, Rectangle toArea, bool enabled, IMonitor monitor, AssetLoader assetLoader)
         {
             // init
             this.ContentPack = contentPack;
@@ -63,6 +67,7 @@ namespace ContentPatcher.Framework.Patchers
             this.FromLocalAsset = fromLocalAsset;
             this.FromArea = fromArea != Rectangle.Empty ? fromArea : null as Rectangle?;
             this.ToArea = toArea != Rectangle.Empty ? toArea : null as Rectangle?;
+            this.Enabled = enabled;
             this.Monitor = monitor;
             this.AssetLoader = assetLoader;
         }
@@ -72,29 +77,33 @@ namespace ContentPatcher.Framework.Patchers
         /// <param name="asset">The asset to edit.</param>
         public void Apply<T>(IAssetData asset)
         {
-            // validate
-            if (typeof(T) != typeof(Texture2D))
+            // changes only if enabled
+            if (this.Enabled)
             {
-                this.Monitor.Log($"Can't apply edit-image patch by {this.ContentPack.Manifest.Name} to {this.AssetName}: this file isn't an image file (found {typeof(T)}).", LogLevel.Warn);
-                return;
-            }
+                // validate
+                if (typeof(T) != typeof(Texture2D))
+                {
+                    this.Monitor.Log($"Can't apply edit-image patch by {this.ContentPack.Manifest.Name} to {this.AssetName}: this file isn't an image file (found {typeof(T)}).", LogLevel.Warn);
+                    return;
+                }
 
-            // fetch data
-            Texture2D source = this.AssetLoader.Load<Texture2D>(this.ContentPack, this.FromLocalAsset);
-            IAssetDataForImage editor = asset.AsImage();
+                // fetch data
+                Texture2D source = this.AssetLoader.Load<Texture2D>(this.ContentPack, this.FromLocalAsset);
+                IAssetDataForImage editor = asset.AsImage();
 
-            // extend tilesheet if needed
-            Rectangle affectedArea = this.GetTargetArea(source, this.FromArea, this.ToArea);
-            if (affectedArea.Bottom > editor.Data.Height)
-            {
-                Texture2D original = editor.Data;
-                Texture2D texture = new Texture2D(Game1.graphics.GraphicsDevice, original.Width, affectedArea.Bottom);
-                editor.ReplaceWith(texture);
-                editor.PatchImage(original);
-            }
+                // extend tilesheet if needed
+                Rectangle affectedArea = this.GetTargetArea(source, this.FromArea, this.ToArea);
+                if (affectedArea.Bottom > editor.Data.Height)
+                {
+                    Texture2D original = editor.Data;
+                    Texture2D texture = new Texture2D(Game1.graphics.GraphicsDevice, original.Width, affectedArea.Bottom);
+                    editor.ReplaceWith(texture);
+                    editor.PatchImage(original);
+                }
 
             // apply source image
             editor.PatchImage(source, this.FromArea, this.ToArea);
+            }
         }
 
 
@@ -107,12 +116,17 @@ namespace ContentPatcher.Framework.Patchers
         /// <param name="toArea">The sprite area to overwrite.</param>
         private Rectangle GetTargetArea(Texture2D source, Rectangle? fromArea, Rectangle? toArea)
         {
-            if (toArea.HasValue)
-                return toArea.Value;
+            if (this.Enabled)
+            {
+                if (toArea.HasValue)
+                    return toArea.Value;
 
-            return fromArea.HasValue
-                ? new Rectangle(0, 0, fromArea.Value.Width, fromArea.Value.Height)
-                : new Rectangle(0, 0, source.Width, source.Height);
+                return fromArea.HasValue
+                    ? new Rectangle(0, 0, fromArea.Value.Width, fromArea.Value.Height)
+                    : new Rectangle(0, 0, source.Width, source.Height);
+            }
+            else
+                return new Rectangle(0, 0, 0, 0);       
         }
     }
 }
