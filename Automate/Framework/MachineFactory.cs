7107618d762357790a75a2fde1ebf3cde559e02e
Jesse Plamondon-Willard <github@jplamondonw.com>
2018-05-15 00:19:30 -0400

    fix incorrect chest ignore logic in Automate

--- Automate/Framework/MachineFactory.cs
+++ Automate/Framework/MachineFactory.cs
@@ -92,7 +92,7 @@ namespace Pathoschild.Stardew.Automate.Framework
                     group.Add(machine);
                     foundSize = size;
                 }
-                else if (this.TryGetChest(location, tile, out Chest chest) && chest.Name.IndexOf("|automate:ignore|", StringComparison.InvariantCultureIgnoreCase) >= 0)
+                else if (this.TryGetChest(location, tile, out Chest chest) && chest.Name.IndexOf("|automate:ignore|", StringComparison.InvariantCultureIgnoreCase) == -1)
                 {
                     group.Add(new ChestContainer(chest));
                     foundSize = Vector2.One;
