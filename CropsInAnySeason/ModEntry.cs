378a90cc885033254ea032aad61f6a83c24de0f4
Jesse Plamondon-Willard <github@jplamondonw.com>
2018-10-06 02:48:17 -0400

    add basic crops in any season logic (#331)

--- CropsInAnySeason/ModEntry.cs
+++ CropsInAnySeason/ModEntry.cs
@@ -1,10 +1,28 @@
+using System.Collections.Generic;
+using Microsoft.Xna.Framework.Graphics;
+using Pathoschild.Stardew.Common.Utilities;
 using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
 
 namespace Pathoschild.Stardew.CropsInAnySeason
 {
     /// <summary>The mod entry point.</summary>
-    internal class ModEntry : Mod
+    internal class ModEntry : Mod, IAssetEditor
     {
+        /*********
+        ** Properties
+        *********/
+        /// <summary>The locations which have been greenhouseified.</summary>
+        private readonly HashSet<GameLocation> ChangedLocations = new HashSet<GameLocation>(new ObjectReferenceComparer<GameLocation>());
+
+        /// <summary>The asset name for the crop data.</summary>
+        private const string CropAssetName = "Data/Crops";
+
+        /// <summary>The asset name for the winter dirt texture.</summary>
+        private const string WinterDirtAssetName = "TerrainFeatures/hoeDirtSnow";
+
+
         /*********
         ** Public methods
         *********/
@@ -12,6 +30,101 @@ namespace Pathoschild.Stardew.CropsInAnySeason
         /// <param name="helper">Provides simplified APIs for writing mods.</param>
         public override void Entry(IModHelper helper)
         {
+            // hook events
+            helper.Events.GameLoop.DayStarted += this.OnDayStarted;
+            helper.Events.World.LocationListChanged += this.OnLocationListChanged;
+            helper.Events.GameLoop.Saving += this.OnSaving;
+        }
+
+        /// <summary>Get whether this instance can edit the given asset.</summary>
+        /// <param name="asset">Basic metadata about the asset being loaded.</param>
+        public bool CanEdit<T>(IAssetInfo asset)
+        {
+            return
+                asset.AssetNameEquals(ModEntry.CropAssetName)
+                || asset.AssetNameEquals(ModEntry.WinterDirtAssetName);
+        }
+
+        /// <summary>Edit a matched asset.</summary>
+        /// <param name="asset">A helper which encapsulates metadata about an asset and enables changes to it.</param>
+        public void Edit<T>(IAssetData asset)
+        {
+            // change crop seasons
+            if (asset.AssetNameEquals(ModEntry.CropAssetName))
+            {
+                asset
+                    .AsDictionary<int, string>()
+                    .Set((id, data) =>
+                    {
+                        string[] fields = data.Split('/');
+                        fields[1] = "spring summer fall winter";
+                        return string.Join("/", fields);
+                    });
+            }
+
+            // change dirt texture
+            else if (asset.AssetNameEquals(ModEntry.WinterDirtAssetName))
+                asset.ReplaceWith(this.Helper.Content.Load<Texture2D>("TerrainFeatures/hoeDirt", ContentSource.GameContent));
+        }
+
+
+        /*********
+        ** Private methods
+        *********/
+        /****
+        ** Event handlers
+        ****/
+        /// <summary>The method called after a new day starts.</summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event arguments.</param>
+        private void OnDayStarted(object sender, DayStartedEventArgs e)
+        {
+            // apply changes
+            this.ChangedLocations.Clear(); // ignore any changes from returning to title
+            this.ApplyChanges(Game1.locations);
+        }
+
+        /// <summary>The method called after a location is added or removed.</summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event arguments.</param>
+        private void OnLocationListChanged(object sender, LocationListChangedEventArgs e)
+        {
+            // handle locations added after day start
+            this.ApplyChanges(e.Added);
+        }
+
+        /// <summary>The method called before the day ends, before the game sets up the next day.</summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event arguments.</param>
+        private void OnSaving(object sender, SavingEventArgs e)
+        {
+            // remove changes before save (to avoid making changes permanent if the mod is uninstalled)
+            this.ClearChanges();
+        }
+
+        /****
+        ** Methods
+        ****/
+        /// <summary>Apply changes to the given locations and track changes for later resetting.</summary>
+        /// <param name="locations">The locations to handle.</param>
+        private void ApplyChanges(IEnumerable<GameLocation> locations)
+        {
+            foreach (GameLocation location in locations)
+            {
+                if (this.ChangedLocations.Contains(location) || !location.IsOutdoors || location.IsGreenhouse)
+                    continue;
+
+                location.IsGreenhouse = true;
+                this.ChangedLocations.Add(location);
+            }
+        }
+
+        /// <summary>Reset changes to all locations.</summary>
+        private void ClearChanges()
+        {
+            foreach (GameLocation location in this.ChangedLocations)
+                location.IsGreenhouse = false;
+            this.ChangedLocations.Clear();
         }
     }
 }
---
+++ CropsInAnySeason/README.md
@@ -0,0 +1,29 @@
+**Crops In Any Season** is a [Stardew Valley](http://stardewvalley.net/) mod that lets you grow
+crops in any season, including winter:
+
+![](screenshot.png)
+
+## Contents
+* [Install](#install)
+* [Compatibility](#compatibility)
+* [See also](#see-also)
+
+## Install
+1. [Install the latest version of SMAPI](https://smapi.io/).
+2. ~~Install this mod from Nexus mods~~.
+3. Run the game using SMAPI.
+
+Note that the mod doesn't change store inventories, so you can only buy crop seeds during their
+usual seasons.
+
+## Compatibility
+The mod is compatible with Stardew Valley 1.3+ on Linux/Mac/Windows, both single-player and
+multiplayer. In multiplayer mode, it must be installed by the main player to work correctly.
+
+## See also
+* [Release notes](release-notes.md)
+* ~~Nexus mod~~
+* ~~Discussion thread~~
+* Inspired by [All Crops All Seasons](https://www.nexusmods.com/stardewvalley/mods/170),
+  which I maintained for a long time after its author left. (This mod uses a very different
+  approach under the hood though.)
