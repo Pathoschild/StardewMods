c37edfa7ac0458740c4c9b81123ddffac6bfe667
Jesse Plamondon-Willard <Pathoschild@users.noreply.github.com>
2019-05-05 16:30:43 -0400

    remove now-unused GlobalAssemblyInfo

--- GlobalAssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-using System.Reflection;
-using System.Runtime.CompilerServices;
-
-[assembly: AssemblyProduct("Pathoschild.Stardew")]
-[assembly: InternalsVisibleTo("Pathoschild.Stardew.Tests.Mods")]
