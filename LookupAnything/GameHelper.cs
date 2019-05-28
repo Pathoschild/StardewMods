3aa9365c029222161c82d9656b32e69dade5462b
Jesse Plamondon-Willard <Pathoschild@users.noreply.github.com>
2019-05-27 22:53:59 -0400

    fix clothing sprites in lookups (#403)

--- LookupAnything/GameHelper.cs
+++ LookupAnything/GameHelper.cs
@@ -522,6 +522,19 @@ namespace Pathoschild.Stardew.LookupAnything
                 return new SpriteInfo(Game1.objectSpriteSheet, Game1.getSourceRectForStandardTileSheet(Game1.objectSpriteSheet, indexInTileSheet, SObject.spriteSheetTileSize, SObject.spriteSheetTileSize));
             }
 
+            // clothing
+            if (item is Clothing clothing)
+            {
+                switch (clothing.clothesType.Value)
+                {
+                    case (int)Clothing.ClothesType.SHIRT:
+                        return new SpriteInfo(FarmerRenderer.shirtsTexture, new Rectangle(clothing.indexInTileSheetMale.Value * 8 % 128, clothing.indexInTileSheetMale.Value * 8 / 128 * 32, 8, 8));
+
+                    case (int)Clothing.ClothesType.PANTS:
+                        return new SpriteInfo(FarmerRenderer.pantsTexture, new Rectangle(192 * (clothing.indexInTileSheetMale.Value % (FarmerRenderer.pantsTexture.Width / 192)), 688 * (clothing.indexInTileSheetMale.Value / (FarmerRenderer.pantsTexture.Width / 192)) + 672, 16, 16));
+                }
+            }
+
             // unknown item
             return null;
         }
