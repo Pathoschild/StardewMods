3f58c1e2aa9ef5ae0cab8a59f7af0150ea5a79a5
Jesse Plamondon-Willard <Pathoschild@users.noreply.github.com>
2019-12-08 00:50:29 -0500

    update version for new feature, add migration

--- ContentPatcher/ModEntry.cs
+++ ContentPatcher/ModEntry.cs
@@ -38,7 +38,7 @@ namespace ContentPatcher
         private readonly string ConfigFileName = "config.json";
 
         /// <summary>The supported format versions.</summary>
-        private readonly string[] SupportedFormatVersions = { "1.0.0", "1.3.0", "1.4.0", "1.5.0", "1.6.0", "1.7.0", "1.8.0", "1.9.0", "1.10.0" };
+        private readonly string[] SupportedFormatVersions = { "1.0.0", "1.3.0", "1.4.0", "1.5.0", "1.6.0", "1.7.0", "1.8.0", "1.9.0", "1.10.0", "1.11.0" };
 
         /// <summary>The format version migrations to apply.</summary>
         private readonly Func<IMigration[]> Migrations = () => new IMigration[]
@@ -50,7 +50,8 @@ namespace ContentPatcher
             new Migration_1_7(),
             new Migration_1_8(),
             new Migration_1_9(),
-            new Migration_1_10()
+            new Migration_1_10(),
+            new Migration_1_11()
         };
 
         /// <summary>The special validation logic to apply to assets affected by patches.</summary>
