429ac8d86ffefe1c20937c1884ab3358dfad627c
Jesse Plamondon-Willard <Pathoschild@users.noreply.github.com>
2020-04-28 17:31:44 -0400

    move item scanning into its own class

--- LookupAnything/GameHelper.cs
+++ LookupAnything/GameHelper.cs
@@ -14,7 +14,6 @@ using Pathoschild.Stardew.LookupAnything.Framework.Models;
 using Pathoschild.Stardew.LookupAnything.Framework.Models.FishData;
 using StardewModdingAPI;
 using StardewValley;
-using StardewValley.Buildings;
 using StardewValley.Characters;
 using StardewValley.GameData.Crafting;
 using StardewValley.GameData.FishPond;
@@ -49,6 +48,9 @@ namespace Pathoschild.Stardew.LookupAnything
 
         /// <summary>Parses the raw game data into usable models.</summary>
         private readonly DataParser DataParser;
+
+        /// <summary>Scans the game world for owned items.</summary>
+        private readonly WorldItemScanner WorldItemScanner = new WorldItemScanner();
 
 
         /*********
@@ -135,124 +137,7 @@ namespace Pathoschild.Stardew.LookupAnything
         /// </remarks>
         public IEnumerable<Item> GetAllOwnedItems()
         {
-            List<Item> items = new List<Item>();
-
-            // in locations
-            foreach (GameLocation location in CommonHelper.GetLocations())
-            {
-                // furniture
-                if (location is DecoratableLocation decorableLocation)
-                {
-                    foreach (Furniture furniture in decorableLocation.furniture)
-                    {
-                        items.Add(furniture);
-
-                        if (furniture is StorageFurniture dresser)
-                            items.AddRange(dresser.heldItems);
-                        else
-                            items.Add(furniture.heldObject.Value);
-                    }
-                }
-
-                // farmhouse fridge
-                if (location is FarmHouse house)
-                    items.AddRange(house.fridge.Value.items);
-
-                // character hats
-                foreach (NPC npc in location.characters)
-                {
-                    items.Add(
-                        (npc as Child)?.hat.Value
-                        ?? (npc as Horse)?.hat.Value
-                    );
-                }
-
-                // building output
-                if (location is BuildableGameLocation buildableLocation)
-                {
-                    foreach (var building in buildableLocation.buildings)
-                    {
-                        if (building is Mill mill)
-                            items.AddRange(mill.output.Value.items);
-                        else if (building is JunimoHut hut)
-                            items.AddRange(hut.output.Value.items);
-                    }
-                }
-
-                // map objects
-                foreach (SObject item in location.objects.Values)
-                {
-                    // chest
-                    if (item is Chest chest)
-                    {
-                        if (chest.playerChest.Value)
-                        {
-                            items.Add(chest);
-                            items.AddRange(chest.items);
-                        }
-                    }
-
-                    // auto-grabber
-                    else if (item.ParentSheetIndex == 165 && item.heldObject.Value is Chest grabberChest)
-                    {
-                        items.Add(item);
-                        items.AddRange(grabberChest.items);
-                    }
-
-                    // craftable
-                    else if (item.bigCraftable.Value)
-                    {
-                        items.Add(item);
-                        if (item.MinutesUntilReady == 0 || item is Cask) // cask output can be retrieved anytime
-                            items.Add(item.heldObject.Value);
-                    }
-
-                    // anything else
-                    else if (!this.IsSpawnedWorldItem(item))
-                    {
-                        items.Add(item);
-                        items.Add(item.heldObject.Value);
-                    }
-                }
-            }
-
-            // inventory
-            items.AddRange(Game1.player.Items);
-            items.AddRange(new Item[]
-            {
-                Game1.player.shirtItem.Value,
-                Game1.player.pantsItem.Value,
-                Game1.player.boots.Value,
-                Game1.player.hat.Value,
-                Game1.player.leftRing.Value,
-                Game1.player.rightRing.Value
-            });
-
-            // hay in silos
-            int hayCount = Game1.getFarm()?.piecesOfHay.Value ?? 0;
-            while (hayCount > 0)
-            {
-                SObject hay = new SObject(178, 1);
-                hay.Stack = Math.Min(hayCount, hay.maximumStackSize());
-                hayCount -= hay.Stack;
-                items.Add(hay);
-            }
-
-            return items.Where(p => p != null);
-        }
-
-        /// <summary>Get whether an item was spawned automatically. This is heuristic and only applies for items placed in the world, not items in an inventory.</summary>
-        /// <param name="item">The item to check.</param>
-        /// <remarks>Derived from the <see cref="SObject"/> constructors.</remarks>
-        public bool IsSpawnedWorldItem(Item item)
-        {
-            return
-                item is SObject obj
-                && (
-                    obj.IsSpawnedObject
-                    || obj.isForage(null) // location argument is only used to check if it's on the beach, in which case everything is forage
-                    || (!(obj is Chest) && (obj.Name == "Weeds" || obj.Name == "Stone" || obj.Name == "Twig"))
-                );
+            return this.WorldItemScanner.GetAllOwnedItems();
         }
 
         /// <summary>Get all NPCs currently in the world.</summary>
