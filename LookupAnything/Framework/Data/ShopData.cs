8480d9a97cc5e5a97dcbf22518df0cdfee96360a
Jesse Plamondon-Willard <Pathoschild@users.noreply.github.com>
2020-10-10 12:53:46 -0400

    remove unused field

--- LookupAnything/Framework/Data/ShopData.cs
+++ LookupAnything/Framework/Data/ShopData.cs
@@ -1,4 +1,4 @@
-﻿namespace Pathoschild.Stardew.LookupAnything.Framework.Data
+namespace Pathoschild.Stardew.LookupAnything.Framework.Data
 {
     /// <summary>Metadata for a shop that isn't available from the game data directly.</summary>
     internal class ShopData
@@ -6,9 +6,6 @@
         /*********
         ** Accessors
         *********/
-        /// <summary>The internal name of the shop's indoor location.</summary>
-        public string LocationName { get; set; }
-
         /// <summary>The translation key for the shop name.</summary>
         public string DisplayKey { get; set; }
 
