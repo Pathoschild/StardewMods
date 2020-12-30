ed18107b12c61c269614266f53a27a74cf62a350
Jesse Plamondon-Willard <Pathoschild@users.noreply.github.com>
2020-12-30 10:53:16 -0500

    add initial project for Horse Flute Anywhere

---
+++ HorseFluteAnywhere/ModEntry.cs
@@ -0,0 +1,17 @@
+using StardewModdingAPI;
+
+namespace Pathoschild.Stardew.HorseFluteAnywhere
+{
+    /// <summary>The mod entry point.</summary>
+    internal class ModEntry : Mod
+    {
+        /*********
+        ** Public methods
+        *********/
+        /// <summary>The mod entry point, called after the mod is first loaded.</summary>
+        /// <param name="helper">Provides methods for interacting with the mod directory, such as read/writing a config file or custom JSON files.</param>
+        public override void Entry(IModHelper helper)
+        {
+        }
+    }
+}
