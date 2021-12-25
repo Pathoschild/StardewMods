992e4db5b52297b51413df804bd67487ea03528c
Jesse Plamondon-Willard <Pathoschild@users.noreply.github.com>
2021-12-25 11:48:18 -0500

    add patch.LastChangedTick field to support Content Patcher Animations

--- ContentPatcher/Framework/Patches/IPatch.cs
+++ ContentPatcher/Framework/Patches/IPatch.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using ContentPatcher.Framework.Conditions;
 using StardewModdingAPI;
+using StardewValley;
 
 namespace ContentPatcher.Framework.Patches
 {
@@ -46,6 +47,9 @@ namespace ContentPatcher.Framework.Patches
 
         /// <summary>Whether the patch is currently applied to the target asset.</summary>
         bool IsApplied { get; set; }
+
+        /// <summary>The <see cref="Game1.ticks"/> value when this patch last changed due to a context update.</summary>
+        int LastChangedTick { get; }
 
 
         /*********
--- ContentPatcher/docs/release-notes.md
+++ ContentPatcher/docs/release-notes.md
@@ -12,6 +12,7 @@ When releasing a format change, don't forget to update the smapi.io/json schema!
 ## Upcoming release
 * Fixed location tokens unready during part of the save loading process.
 * Fixed minimum supported Generic Mod Config Menu version.
+* Internal changes to support Content Patcher Animations.
 
 ## 1.24.5
 Released 30 November 2021 for SMAPI 3.13.0 or later.
