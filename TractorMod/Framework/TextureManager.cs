d3729609e2628fb8d47fe250179484587b5fb2ca
Jesse Plamondon-Willard <Pathoschild@users.noreply.github.com>
2022-03-04 00:31:26 -0500

    encapsulate Tractor Mod's texture handling

---
+++ TractorMod/Framework/TextureManager.cs
@@ -0,0 +1,247 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.Xna.Framework.Graphics;
+using StardewModdingAPI;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+using StardewValley.Buildings;
+using StardewValley.Characters;
+using StardewValley.Menus;
+
+namespace Pathoschild.Stardew.TractorMod.Framework
+{
+    /// <summary>Manages textures loaded for the tractor and garage.</summary>
+    internal class TextureManager : IAssetLoader, IDisposable
+    {
+        /*********
+        ** Fields
+        *********/
+        /// <summary>The absolute path to the Tractor Mod folder.</summary>
+        private readonly string DirectoryPath;
+
+        /// <summary>The base path for assets loaded through the game's content pipeline so other mods can edit them.</summary>
+        private readonly string PublicAssetBasePath;
+
+        /// <summary>The content helper from which to load assets.</summary>
+        private readonly IContentHelper ContentHelper;
+
+        /// <summary>The monitor with which to log errors.</summary>
+        private readonly IMonitor Monitor;
+
+        /// <summary>A case-insensitive list of files in the <c>assets</c> folder.</summary>
+        private readonly IDictionary<string, string> AssetMap;
+
+
+        /*********
+        ** Accessors
+        *********/
+        /// <summary>The garage texture to apply.</summary>
+        public Texture2D GarageTexture { get; private set; }
+
+        /// <summary>The tractor texture to apply.</summary>
+        public Texture2D TractorTexture { get; private set; }
+
+
+        /*********
+        ** Public methods
+        *********/
+        /// <summary>Construct an instance.</summary>
+        /// <param name="directoryPath">The absolute path to the Tractor Mod folder.</param>
+        /// <param name="publicAssetBasePath">The base path for assets loaded through the game's content pipeline so other mods can edit them.</param>
+        /// <param name="contentHelper">The content helper from which to load assets.</param>
+        /// <param name="monitor">The monitor with which to log errors.</param>
+        public TextureManager(string directoryPath, string publicAssetBasePath, IContentHelper contentHelper, IMonitor monitor)
+        {
+            this.DirectoryPath = directoryPath;
+            this.PublicAssetBasePath = publicAssetBasePath;
+            this.ContentHelper = contentHelper;
+            this.Monitor = monitor;
+
+            this.AssetMap = this.BuildAssetMap(directoryPath);
+        }
+
+        /// <summary>Update the textures if needed.</summary>
+        public void UpdateTextures()
+        {
+            // tractor
+            if (this.TryLoadFromContent("tractor", out Texture2D texture, out string error))
+                this.TractorTexture = texture;
+            else
+                this.Monitor.Log(error, LogLevel.Error);
+
+            // garage
+            if (this.TryLoadFromContent("garage", out texture, out error))
+                this.GarageTexture = texture;
+            else
+                this.Monitor.Log(error, LogLevel.Error);
+        }
+
+        /// <summary>Apply the mod textures to the given menu, if applicable.</summary>
+        /// <param name="menu">The menu to change.</param>
+        /// <param name="isFarmExpansion">Whether the menu is the Farm Expansion build menu.</param>
+        /// <param name="isPelicanFiber">Whether the menu is the Pelican Fiber build menu.</param>
+        /// <param name="isGarage">Whether a blueprint is for a tractor garage.</param>
+        /// <param name="reflection">The SMAPI API for accessing internal code.</param>
+        public void ApplyTextures(IClickableMenu menu, bool isFarmExpansion, bool isPelicanFiber, Func<BluePrint, bool> isGarage, IReflectionHelper reflection)
+        {
+            // vanilla menu
+            if (menu is CarpenterMenu carpenterMenu)
+            {
+                if (isGarage(carpenterMenu.CurrentBlueprint))
+                {
+                    Building building = reflection.GetField<Building>(carpenterMenu, "currentBuilding").GetValue();
+                    if (building.texture.Value != this.GarageTexture && this.GarageTexture != null)
+                        building.texture = new Lazy<Texture2D>(() => this.GarageTexture);
+                }
+                return;
+            }
+
+            // Farm Expansion & Pelican Fiber menus
+
+            if (isFarmExpansion || isPelicanFiber)
+            {
+                BluePrint currentBlueprint = reflection.GetProperty<BluePrint>(menu, isFarmExpansion ? "CurrentBlueprint" : "currentBlueprint").GetValue();
+                if (isGarage(currentBlueprint))
+                {
+                    Building building = reflection.GetField<Building>(menu, "currentBuilding").GetValue();
+                    if (building.texture.Value != this.GarageTexture && this.GarageTexture != null)
+                        building.texture = new Lazy<Texture2D>(() => this.GarageTexture);
+                }
+            }
+        }
+
+        /// <summary>Apply the mod textures to the given stable, if applicable.</summary>
+        /// <param name="horse">The horse to change.</param>
+        /// <param name="isTractor">Get whether a horse is a tractor.</param>
+        public void ApplyTextures(Horse horse, Func<Horse, bool> isTractor)
+        {
+            if (this.TractorTexture != null && isTractor(horse))
+                horse.Sprite.spriteTexture = this.TractorTexture;
+        }
+
+        /// <summary>Apply the mod textures to the given stable, if applicable.</summary>
+        /// <param name="stable">The stable to change.</param>
+        /// <param name="isGarage">Get whether a stable is a garage.</param>
+        public void ApplyTextures(Stable stable, Func<Stable, bool> isGarage)
+        {
+            if (this.GarageTexture != null && isGarage(stable))
+                stable.texture = new Lazy<Texture2D>(() => this.GarageTexture);
+        }
+
+        /// <inheritdoc />
+        public void Dispose()
+        {
+            this.GarageTexture?.Dispose();
+            this.TractorTexture?.Dispose();
+        }
+
+
+        /*********
+        ** Private methods
+        *********/
+        /// <inheritdoc />
+        bool IAssetLoader.CanLoad<T>(IAssetInfo asset)
+        {
+            return
+                asset.AssetNameEquals("Buildings/TractorGarage")
+                || asset.AssetNameEquals($"{this.PublicAssetBasePath}/Tractor")
+                || asset.AssetNameEquals($"{this.PublicAssetBasePath}/Garage");
+        }
+
+        /// <inheritdoc />
+        T IAssetLoader.Load<T>(IAssetInfo asset)
+        {
+            // Allow for garages from older versions that didn't get normalized correctly.
+            // This can be removed once support for legacy data is dropped.
+            if (asset.AssetNameEquals($"Buildings/TractorGarage"))
+                return (T)(object)this.GarageTexture;
+
+            // load tractor or garage texture
+            string key = PathUtilities.GetSegments(asset.AssetName).Last();
+            return this.TryLoadFromFile(key, out Texture2D texture, out string error)
+                ? (T)(object)texture
+                : throw new InvalidOperationException(error);
+        }
+
+        /// <summary>Try to load the asset for a texture from the game's content folder so other mods can apply edits.</summary>
+        /// <param name="spritesheet">The spritesheet name without the path or extension (like 'Tractor' or 'Garage').</param>
+        /// <param name="texture">The loaded texture, if found.</param>
+        /// <param name="error">A human-readable error to show to the user if texture wasn't found.</param>
+        private bool TryLoadFromContent(string spritesheet, out Texture2D texture, out string error)
+        {
+            try
+            {
+                texture = Game1.content.Load<Texture2D>($"{this.PublicAssetBasePath}/{spritesheet}");
+                error = null;
+            }
+            catch (Exception ex)
+            {
+                texture = null;
+                error = ex.ToString();
+                return false;
+            }
+
+            return texture != null;
+        }
+
+        /// <summary>Try to load the asset for a texture from the assets folder (including seasonal logic if applicable).</summary>
+        /// <param name="spritesheet">The spritesheet name without the path or extension (like 'tractor' or 'garage').</param>
+        /// <param name="texture">The loaded texture, if found.</param>
+        /// <param name="error">A human-readable error to show to the user if texture wasn't found.</param>
+        private bool TryLoadFromFile(string spritesheet, out Texture2D texture, out string error)
+        {
+            texture = this.TryGetTextureKey(spritesheet, out string key, out error)
+                ? this.ContentHelper.Load<Texture2D>(key)
+                : null;
+
+            return texture != null;
+        }
+
+        /// <summary>Try to get the asset key for a texture from the assets folder (including seasonal logic if applicable).</summary>
+        /// <param name="spritesheet">The spritesheet name without the path or extension (like 'tractor' or 'garage').</param>
+        /// <param name="key">The asset key to use, if found.</param>
+        /// <param name="error">A human-readable error to show to the user if texture wasn't found.</param>
+        private bool TryGetTextureKey(string spritesheet, out string key, out string error)
+        {
+            string seasonalKey = $"{Game1.currentSeason}_{spritesheet}.png";
+            string defaultKey = $"{spritesheet}.png";
+
+            foreach (string possibleKey in new[] { seasonalKey, defaultKey })
+            {
+                if (this.AssetMap.TryGetValue(possibleKey, out string actualKey) && File.Exists(Path.Combine(this.DirectoryPath, $"assets/{actualKey}")))
+                {
+                    key = $"assets/{actualKey}";
+                    error = null;
+                    return true;
+                }
+            }
+
+            key = null;
+            error = $"Couldn't find file '{defaultKey}' in the mod folder. This mod isn't installed correctly; try reinstalling the mod to fix it.";
+            return false;
+        }
+
+        /// <summary>Initialize a case-insensitive list of files in the <c>assets</c> folder.</summary>
+        /// <param name="directoryPath">The absolute path to the mod folder.</param>
+        private IDictionary<string, string> BuildAssetMap(string directoryPath)
+        {
+            var assetMap = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+
+            // get assets folder
+            DirectoryInfo dir = new DirectoryInfo(Path.Combine(directoryPath, "assets"));
+            if (!dir.Exists)
+            {
+                this.Monitor.Log("Tractor Mod's 'assets' folder is missing. The mod will not work correctly. Try reinstalling the mod to fix this.", LogLevel.Error);
+                return assetMap;
+            }
+
+            // create map
+            foreach (FileInfo file in dir.GetFiles())
+                assetMap[file.Name] = file.Name;
+
+            return assetMap;
+        }
+    }
+}
--- TractorMod/ModEntry.cs
+++ TractorMod/ModEntry.cs
@@ -1,9 +1,7 @@
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
 using Pathoschild.Stardew.Common;
 using Pathoschild.Stardew.Common.Integrations.FarmExpansion;
 using Pathoschild.Stardew.Common.Utilities;
@@ -23,7 +21,7 @@ using StardewValley.Menus;
 namespace Pathoschild.Stardew.TractorMod
 {
     /// <summary>The mod entry point.</summary>
-    internal class ModEntry : Mod, IAssetLoader
+    internal class ModEntry : Mod
     {
         /*********
         ** Fields
@@ -58,9 +56,6 @@ namespace Pathoschild.Stardew.TractorMod
         /// <summary>The message ID for a request to warp a tractor to the given farmhand.</summary>
         private readonly string RequestTractorMessageID = "TractorRequest";
 
-        /// <summary>A case-insensitive list of files in the <c>assets</c> folder.</summary>
-        private readonly IDictionary<string, string> AssetMap = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
-
         /****
         ** State
         ****/
@@ -70,17 +65,14 @@ namespace Pathoschild.Stardew.TractorMod
         /// <summary>The configured key bindings.</summary>
         private ModConfigKeys Keys => this.Config.Controls;
 
+        /// <summary>Manages textures loaded for the tractor and garage.</summary>
+        private TextureManager TextureManager;
+
         /// <summary>The backing field for <see cref="TractorManager"/>.</summary>
         private PerScreen<TractorManager> TractorManagerImpl;
 
         /// <summary>The tractor being ridden by the current player.</summary>
         private TractorManager TractorManager => this.TractorManagerImpl.Value;
-
-        /// <summary>The garage texture to apply.</summary>
-        private Texture2D GarageTexture;
-
-        /// <summary>The tractor texture to apply.</summary>
-        private Texture2D TractorTexture;
 
         /// <summary>Whether the mod is enabled for the current farmhand.</summary>
         private bool IsEnabled = true;
@@ -98,6 +90,12 @@ namespace Pathoschild.Stardew.TractorMod
 
             // init
             I18n.Init(helper.Translation);
+            this.TextureManager = new(
+                directoryPath: this.Helper.DirectoryPath,
+                publicAssetBasePath: this.PublicAssetBasePath,
+                contentHelper: helper.Content,
+                monitor: this.Monitor
+            );
             this.TractorManagerImpl = new(() =>
             {
                 var manager = new TractorManager(this.Config, this.Keys, this.Helper.Reflection);
@@ -105,7 +103,9 @@ namespace Pathoschild.Stardew.TractorMod
                 return manager;
             });
             this.UpdateConfig();
-            this.InitAssetMap();
+
+            // hook assets
+            helper.Content.AssetLoaders.Add(this.TextureManager);
 
             // hook events
             IModEvents events = helper.Events;
@@ -126,32 +126,6 @@ namespace Pathoschild.Stardew.TractorMod
             // validate translations
             if (!helper.Translation.GetTranslations().Any())
                 this.Monitor.Log("The translation files in this mod's i18n folder seem to be missing. The mod will still work, but you'll see 'missing translation' messages. Try reinstalling the mod to fix this.", LogLevel.Warn);
-        }
-
-        /// <summary>Get whether this instance can load the initial version of the given asset.</summary>
-        /// <param name="asset">Basic metadata about the asset being loaded.</param>
-        public bool CanLoad<T>(IAssetInfo asset)
-        {
-            return
-                asset.AssetNameEquals($"Buildings/{this.BlueprintBuildingType}")
-                || asset.AssetNameEquals($"{this.PublicAssetBasePath}/Tractor")
-                || asset.AssetNameEquals($"{this.PublicAssetBasePath}/Garage");
-        }
-
-        /// <summary>Load a matched asset.</summary>
-        /// <param name="asset">Basic metadata about the asset being loaded.</param>
-        public T Load<T>(IAssetInfo asset)
-        {
-            // Allow for garages from older versions that didn't get normalized correctly.
-            // This can be removed once support for legacy data is dropped.
-            if (asset.AssetNameEquals($"Buildings/{this.BlueprintBuildingType}"))
-                return (T)(object)this.GarageTexture;
-
-            // load tractor or garage texture
-            string key = PathUtilities.GetSegments(asset.AssetName).Last();
-            return this.TryLoadFromFile(key, out Texture2D texture, out string error)
-                ? (T)(object)texture
-                : throw new InvalidOperationException(error);
         }
 
 
@@ -231,10 +205,7 @@ namespace Pathoschild.Stardew.TractorMod
                 return;
 
             // reload textures
-            if (!this.TryLoadFromContent("tractor", out this.TractorTexture, out string error))
-                this.Monitor.Log(error, LogLevel.Error);
-            if (!this.TryLoadFromContent("garage", out this.GarageTexture, out error))
-                this.Monitor.Log(error, LogLevel.Error);
+            this.TextureManager.UpdateTextures();
 
             // init garages + tractors
             if (Context.IsMainPlayer)
@@ -266,8 +237,8 @@ namespace Pathoschild.Stardew.TractorMod
                             tractor.ownerId.Value = 0;
 
                         // apply textures
-                        this.ApplyTextures(garage);
-                        this.ApplyTextures(tractor);
+                        this.TextureManager.ApplyTextures(garage, this.IsGarage);
+                        this.TextureManager.ApplyTextures(tractor, this.IsTractor);
                     }
                 }
             }
@@ -350,15 +321,27 @@ namespace Pathoschild.Stardew.TractorMod
                 if (e.IsMultipleOf(updateRate))
                 {
                     foreach (Horse horse in this.GetTractorsIn(Game1.currentLocation))
-                        this.ApplyTextures(horse);
+                        this.TextureManager.ApplyTextures(horse, this.IsTractor);
                     foreach (Stable stable in this.GetGaragesIn(Game1.currentLocation))
-                        this.ApplyTextures(stable);
+                        this.TextureManager.ApplyTextures(stable, this.IsGarage);
                 }
             }
 
             // override blueprint texture
             if (Game1.activeClickableMenu != null)
-                this.ApplyTextures(Game1.activeClickableMenu);
+            {
+                IClickableMenu menu = Game1.activeClickableMenu;
+                bool isFarmExpansion = menu.GetType().FullName == this.FarmExpansionMenuFullName;
+                bool isPelicanFiber = !isFarmExpansion && menu.GetType().FullName == this.PelicanFiberMenuFullName;
+
+                this.TextureManager.ApplyTextures(
+                    menu: menu,
+                    isFarmExpansion: isFarmExpansion,
+                    isPelicanFiber: isPelicanFiber,
+                    isGarage: blueprint => blueprint.maxOccupants == this.MaxOccupantsID,
+                    reflection: this.Helper.Reflection
+                );
+            }
 
             // update tractor effects
             if (Context.IsPlayerFree)
@@ -535,22 +518,6 @@ namespace Pathoschild.Stardew.TractorMod
             });
         }
 
-        /// <summary>Initialize the <see cref="AssetMap"/> from the files on disk.</summary>
-        private void InitAssetMap()
-        {
-            // get assets folder
-            DirectoryInfo dir = new DirectoryInfo(Path.Combine(this.Helper.DirectoryPath, "assets"));
-            if (!dir.Exists)
-            {
-                this.Monitor.Log("Tractor Mod's 'assets' folder is missing. The mod will not work correctly. Try reinstalling the mod to fix this.", LogLevel.Error);
-                return;
-            }
-
-            // create map
-            foreach (FileInfo file in dir.GetFiles())
-                this.AssetMap[file.Name] = file.Name;
-        }
-
         /// <summary>Summon an unused tractor to the player's current position, if any are available.</summary>
         private void SummonTractor()
         {
@@ -596,7 +563,7 @@ namespace Pathoschild.Stardew.TractorMod
             {
                 tractor = new Horse(Guid.NewGuid(), 0, 0);
                 TractorManager.SetTractorInfo(tractor);
-                this.ApplyTextures(tractor);
+                this.TextureManager.ApplyTextures(tractor, this.IsTractor);
             }
 
             // warp to player
@@ -741,111 +708,6 @@ namespace Pathoschild.Stardew.TractorMod
         private Vector2 GetDefaultTractorTile(Stable garage)
         {
             return new Vector2(garage.tileX.Value + 1, garage.tileY.Value + 1);
-        }
-
-        /// <summary>Try to load the asset for a texture from the game's content folder so other mods can apply edits.</summary>
-        /// <param name="spritesheet">The spritesheet name without the path or extension (like 'Tractor' or 'Garage').</param>
-        /// <param name="texture">The loaded texture, if found.</param>
-        /// <param name="error">A human-readable error to show to the user if texture wasn't found.</param>
-        private bool TryLoadFromContent(string spritesheet, out Texture2D texture, out string error)
-        {
-            try
-            {
-                texture = Game1.content.Load<Texture2D>($"{this.PublicAssetBasePath}/{spritesheet}");
-                error = null;
-            }
-            catch (Exception ex)
-            {
-                texture = null;
-                error = ex.ToString();
-                return false;
-            }
-
-            return texture != null;
-        }
-
-        /// <summary>Try to load the asset for a texture from the assets folder (including seasonal logic if applicable).</summary>
-        /// <param name="spritesheet">The spritesheet name without the path or extension (like 'tractor' or 'garage').</param>
-        /// <param name="texture">The loaded texture, if found.</param>
-        /// <param name="error">A human-readable error to show to the user if texture wasn't found.</param>
-        private bool TryLoadFromFile(string spritesheet, out Texture2D texture, out string error)
-        {
-            texture = this.TryGetTextureKey(spritesheet, out string key, out error)
-                ? this.Helper.Content.Load<Texture2D>(key)
-                : null;
-
-            return texture != null;
-        }
-
-        /// <summary>Try to get the asset key for a texture from the assets folder (including seasonal logic if applicable).</summary>
-        /// <param name="spritesheet">The spritesheet name without the path or extension (like 'tractor' or 'garage').</param>
-        /// <param name="key">The asset key to use, if found.</param>
-        /// <param name="error">A human-readable error to show to the user if texture wasn't found.</param>
-        private bool TryGetTextureKey(string spritesheet, out string key, out string error)
-        {
-            string seasonalKey = $"{Game1.currentSeason}_{spritesheet}.png";
-            string defaultKey = $"{spritesheet}.png";
-
-            foreach (string possibleKey in new[] { seasonalKey, defaultKey })
-            {
-                if (this.AssetMap.TryGetValue(possibleKey, out string actualKey) && File.Exists(Path.Combine(this.Helper.DirectoryPath, $"assets/{actualKey}")))
-                {
-                    key = $"assets/{actualKey}";
-                    error = null;
-                    return true;
-                }
-            }
-
-            key = null;
-            error = $"Couldn't find file '{defaultKey}' in the mod folder. This mod isn't installed correctly; try reinstalling the mod to fix it.";
-            return false;
-        }
-
-        /// <summary>Apply the mod textures to the given menu, if applicable.</summary>
-        /// <param name="menu">The menu to change.</param>
-        private void ApplyTextures(IClickableMenu menu)
-        {
-            // vanilla menu
-            if (menu is CarpenterMenu carpenterMenu)
-            {
-                if (carpenterMenu.CurrentBlueprint.maxOccupants == this.MaxOccupantsID)
-                {
-                    Building building = this.Helper.Reflection.GetField<Building>(carpenterMenu, "currentBuilding").GetValue();
-                    if (building.texture.Value != this.GarageTexture && this.GarageTexture != null)
-                        building.texture = new Lazy<Texture2D>(() => this.GarageTexture);
-                }
-                return;
-            }
-
-            // Farm Expansion & Pelican Fiber menus
-            bool isFarmExpansion = menu.GetType().FullName == this.FarmExpansionMenuFullName;
-            bool isPelicanFiber = !isFarmExpansion && menu.GetType().FullName == this.PelicanFiberMenuFullName;
-            if (isFarmExpansion || isPelicanFiber)
-            {
-                BluePrint currentBlueprint = this.Helper.Reflection.GetProperty<BluePrint>(menu, isFarmExpansion ? "CurrentBlueprint" : "currentBlueprint").GetValue();
-                if (currentBlueprint.maxOccupants == this.MaxOccupantsID)
-                {
-                    Building building = this.Helper.Reflection.GetField<Building>(menu, "currentBuilding").GetValue();
-                    if (building.texture.Value != this.GarageTexture && this.GarageTexture != null)
-                        building.texture = new Lazy<Texture2D>(() => this.GarageTexture);
-                }
-            }
-        }
-
-        /// <summary>Apply the mod textures to the given stable, if applicable.</summary>
-        /// <param name="horse">The horse to change.</param>
-        private void ApplyTextures(Horse horse)
-        {
-            if (this.TractorTexture != null && this.IsTractor(horse))
-                horse.Sprite.spriteTexture = this.TractorTexture;
-        }
-
-        /// <summary>Apply the mod textures to the given stable, if applicable.</summary>
-        /// <param name="stable">The stable to change.</param>
-        private void ApplyTextures(Stable stable)
-        {
-            if (this.GarageTexture != null && this.IsGarage(stable))
-                stable.texture = new Lazy<Texture2D>(() => this.GarageTexture);
         }
     }
 }
