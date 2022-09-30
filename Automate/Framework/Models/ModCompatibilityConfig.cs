3d133ec3ba8a06fe9c744cef9a56144bac3b1587
kzawora <konrad.zawora@outlook.com>
2022-10-01 00:13:56 +0200

    Update description

--- Automate/Framework/Models/ModCompatibilityConfig.cs
+++ Automate/Framework/Models/ModCompatibilityConfig.cs
@@ -9,10 +9,10 @@ namespace Pathoschild.Stardew.Automate.Framework.Models
         /// <summary>Whether to enable compatibility with Better Junimos. If it's installed, Junimo huts won't output fertilizer or seeds.</summary>
         public bool BetterJunimos { get; set; } = true;
 
-        /// <summary>Whether to transfer seeds into Junimo huts. If true, Junimo huts will accept seeds from connected chests. Does nothing without Better Junimos mod.</summary>
+        /// <summary>Whether to transfer seeds into Junimo huts. If true, Junimo huts will accept seeds from connected chests. Does nothing without Better Junimos mod compatibility.</summary>
         public bool BetterJunimosTransferSeedsToJunimoHuts { get; set; } = false;
 
-        /// <summary>Whether to transfer fertilizers into Junimo huts. If true, Junimo huts will accept seeds from connected chests. Does nothing without Better Junimos mod.</summary>
+        /// <summary>Whether to transfer fertilizers into Junimo huts. If true, Junimo huts will accept fertilizers from connected chests. Does nothing without Better Junimos mod compatibility.</summary>
         public bool BetterJunimosTransferFertilizersToJunimoHuts { get; set; } = false;
 
         /// <summary>Whether to log a warning if the player installs a custom-machine mod that requires a separate compatibility patch which isn't installed.</summary>
