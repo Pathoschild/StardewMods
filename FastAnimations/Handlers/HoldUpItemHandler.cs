b0a83a40690952cfec9a07574ff5637b91566116
weizinai <1776061683@qq.com>
2024-05-20 11:10:55 +0800

    Speed up Temporary Sprite Animation

--- FastAnimations/Handlers/HoldUpItemHandler.cs
+++ FastAnimations/Handlers/HoldUpItemHandler.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
 using Pathoschild.Stardew.FastAnimations.Framework;
 using StardewValley;
+using StardewValley.Objects;
 
 namespace Pathoschild.Stardew.FastAnimations.Handlers;
 
@@ -28,7 +31,63 @@ internal class HoldUpItemHandler : BaseAnimationHandler
     /// <param name="playerAnimationID">The player's current animation ID.</param>
     public override void Update(int playerAnimationID)
     {
-        this.SpeedUpPlayer(() => !this.IsHoldingUpItem());
+        var player = Game1.player;
+        var location = Game1.currentLocation;
+
+        this.ApplySkips(
+            () =>
+            {
+                player.Update(Game1.currentGameTime, location);
+
+                foreach (var sprite in this.GetTemporarySprites(player).ToArray())
+                {
+                    bool done = sprite.update(Game1.currentGameTime);
+                    if (done)
+                        location.TemporarySprites.Remove(sprite);
+                }
+            },
+            () => !this.IsHoldingUpItem()
+        );
+    }
+
+    /*********
+     ** Private methods
+     *********/
+    /// <summary>Get the temporary animated sprites added as part of the item-holdup animation.</summary>
+    /// <param name="player">The player being animated.</param>
+    /// <remarks>Derived from <see cref="Farmer.showHoldingItem"/>.</remarks>
+    private IEnumerable<TemporaryAnimatedSprite> GetTemporarySprites(Farmer player)
+    {
+        // get hold up item
+        Item? holdingItem = player.mostRecentlyGrabbedItem ?? player.ActiveItem;
+
+        foreach (TemporaryAnimatedSprite sprite in player.currentLocation.TemporarySprites)
+        {
+            switch (holdingItem)
+            {
+                case null:
+                {
+                    if (sprite.textureName == "LooseSprites\\Cursors" && sprite.sourceRect == new Microsoft.Xna.Framework.Rectangle(420, 489, 25, 18))
+                        yield return sprite;
+                    break;
+                }
+                case SpecialItem specialItem:
+                {
+                    if (sprite.textureName == "LooseSprites\\Cursors" &&
+                        (sprite.sourceRect == new Rectangle(Game1.player.MaxItems == 36 ? 268 : 257, 1436, Game1.player.MaxItems == 36 ? 11 : 9, 13) ||
+                         sprite.sourceRect == new Rectangle(129 + 16 * specialItem.which.Value, 320, 16, 16)))
+                        yield return sprite;
+                    break;
+                }
+                default:
+                {
+                    var data = ItemRegistry.GetDataOrErrorItem(holdingItem.QualifiedItemId);
+                    if (sprite.textureName == data.TextureName && sprite.sourceRect == data.GetSourceRect())
+                        yield return sprite;
+                    break;
+                }
+            }
+        }
     }
 
     private bool IsHoldingUpItem()
