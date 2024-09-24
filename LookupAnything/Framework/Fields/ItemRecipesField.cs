0606c76d23880a7bdddc9d4030fee2d34618d83c
mushymato <wildshinobu@gmail.com>
2024-09-24 19:49:56 -0400

    link color should respect known status

--- LookupAnything/Framework/Fields/ItemRecipesField.cs
+++ LookupAnything/Framework/Fields/ItemRecipesField.cs
@@ -151,7 +151,7 @@ namespace Pathoschild.Stardew.LookupAnything.Framework.Fields
                     // fade recipes which aren't known
                     Color iconColor = entry.IsKnown ? Color.White : Color.White * .5f;
                     Color textColor = entry.IsKnown ? Color.Black : Color.Gray;
-                    Color linkColor = Color.Blue;
+                    Color linkColor = entry.IsKnown ? Color.Blue : Color.Blue * .5f;
 
                     // reset position for recipe output
                     float recipeLeftMargin = position.X + firstRecipeLeftMargin;
