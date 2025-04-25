83423ab6f814103a10d0190eb02277f5eca9ff90
Jesse Plamondon-Willard <Pathoschild@users.noreply.github.com>
2025-04-24 22:08:18 -0400

    tweak new code

--- ContentPatcher/Framework/Commands/Commands/ExportCommand.cs
+++ ContentPatcher/Framework/Commands/Commands/ExportCommand.cs
@@ -346,6 +346,7 @@ internal class ExportCommand : BaseCommand
     {
         IAssetName assetName = this.ContentHelper.ParseAssetName(asset);
 
+        // based on path
         if (assetName.IsDirectlyUnderPath("Maps"))
             return [typeof(Map), typeof(Texture2D)];
 
@@ -364,20 +365,20 @@ internal class ExportCommand : BaseCommand
             assetName.IsDirectlyUnderPath("Characters/Dialogue")
             || assetName.IsDirectlyUnderPath("Characters/schedules")
             || assetName.IsDirectlyUnderPath("Data/Events")
-            || assetName.IsDirectlyUnderPath("Data/festivals")
+            || assetName.IsDirectlyUnderPath("Data/Festivals")
         )
             return [typeof(Dictionary<string, string>)];
 
+        // based on DataLoader method
         if (assetName.IsDirectlyUnderPath("Data"))
         {
             string name = assetName.BaseName["Data/".Length..];
-            // No vanilla data asset has `_` in its name, but DataLoader uses it in a few places for subfolder assets, eg 'Festivals_FestivalDates'
-            if (name.Contains('_')) return null;
-            var reflectionType = typeof(DataLoader).GetMethods().FirstOrDefault(type => type.Name.Equals(name, StringComparison.InvariantCultureIgnoreCase));
-            if (reflectionType != null)
-            {
-                return [reflectionType.ReturnType];
-            }
+            if (name.Contains('_'))
+                return null; // no vanilla data asset has `_` in its name, but DataLoader uses it for subfolders like 'Festivals_FestivalDates'
+
+            MethodInfo? method = typeof(DataLoader).GetMethod(name, BindingFlags.Public | BindingFlags.Static | BindingFlags.IgnoreCase);
+            if (method != null)
+                return [method.ReturnType];
         }
 
         return null;
