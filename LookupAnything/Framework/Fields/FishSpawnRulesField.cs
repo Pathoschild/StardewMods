c1de54c59a7a9e7eceac195935f7903b03847632
mushymato <wildshinobu@gmail.com>
2025-04-30 00:32:12 -0400

    fix spawn rule field reporting incorrect HasValue

--- LookupAnything/Framework/Fields/FishSpawnRulesField.cs
+++ LookupAnything/Framework/Fields/FishSpawnRulesField.cs
@@ -76,7 +76,7 @@ internal class FishSpawnRulesField : CheckboxListField
         : base(label)
     {
         this.CheckboxLists = spawnConditions;
-        this.HasValue = this.CheckboxLists.Any();
+        this.HasValue = this.CheckboxLists.Any(checkboxList => checkboxList.Checkboxes.Any());
     }
 
     /// <summary>Get the formatted checkbox conditions for all fish in a location.</summary>
