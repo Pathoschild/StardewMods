7cef286ff5c4c098ae473be3d22298375d2005d6
Banana <banana@gmail.com>
2025-08-18 17:35:36 +0200

    remove imports

--- TractorMod/Framework/Attachments/SeedAttachment.cs
+++ TractorMod/Framework/Attachments/SeedAttachment.cs
@@ -1,8 +1,6 @@
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.Xna.Framework;
-using Pathoschild.Stardew.Common.Items;
 using Pathoschild.Stardew.TractorMod.Framework.Config;
 using StardewModdingAPI;
 using StardewValley;
