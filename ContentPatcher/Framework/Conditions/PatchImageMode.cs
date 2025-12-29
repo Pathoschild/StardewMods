dba7a1d2c36821350cb455ec90dfd011aed4803c
Jesse Plamondon-Willard <Pathoschild@users.noreply.github.com>
2025-12-29 15:37:20 -0500

    add docs & migration for new PatchMode.Mask

--- ContentPatcher/Framework/Conditions/PatchImageMode.cs
+++ ContentPatcher/Framework/Conditions/PatchImageMode.cs
@@ -5,12 +5,12 @@ namespace ContentPatcher.Framework.Conditions;
 /// <summary>Indicates how an image should be patched.</summary>
 public enum PatchImageMode
 {
-    /// <summary>Erase the original content within the area before drawing the new content.</summary>
+    /// <inheritdoc cref="PatchMode.Replace" />
     Replace = PatchMode.Replace,
 
-    /// <summary>Draw the new content over the original content, so the original content shows through any transparent or semi-transparent pixels.</summary>
+    /// <inheritdoc cref="PatchMode.Overlay" />
     Overlay = PatchMode.Overlay,
 
-    /// <summary>Masks the original content so that the alpha value of every pixel in the new content gets subtracted from the corresponding pixel in the original content.</summary>
+    /// <inheritdoc cref="PatchMode.Mask" />
     Mask = PatchMode.Mask
 }
